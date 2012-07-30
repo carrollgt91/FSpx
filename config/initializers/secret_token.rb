0df7e7aef05d1bd3a5d56e2409bb3015b39fc9e4
Grayson Carroll <carrollgt91@gmail.com>
2012-07-30 16:00:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FSpx::Application.config.secret_token = '6b2f0988c4d149bb68cc65b6370b116f1908ff42dd897f7d90a55cbdaa707d6b4f0baacc45acfd780fec15e3eeb46cc35f225716cf28ffeaebc39677af406d9e'
